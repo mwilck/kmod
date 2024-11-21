mwilck/kmod

commit e62d8c725b73cc03efc01df59bf5cc5a5fa31b5d

strbuf: make strbuf_pushchars() a little less dumb
Do not push one char at a time. We have strlen() and memcpy() to rely
on.

shared/strbuf.c
@@ -22,6 +22,7 @@
 #include <assert.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "util.h"
 #include "strbuf.h"
@@ -93,15 +94,20 @@ bool strbuf_pushchar(struct strbuf *buf, char ch)
 
 unsigned strbuf_pushchars(struct strbuf *buf, const char *str)
 {
-	unsigned i = 0;
-	int ch;
+	unsigned int len;
 
-	while ((ch = str[i])) {
-		strbuf_pushchar(buf, ch);
-		i++;
-	}
+	assert(str != NULL);
+	assert(buf != NULL);
 
-	return i;
+	len = strlen(str);
+
+	if (!buf_grow(buf, buf->used + len))
+		return 0;
+
+	memcpy(buf->bytes + buf->used, str, len);
+	buf->used += len;
+
+	return len;
 }
 
 void strbuf_popchar(struct strbuf *buf)

commit 3a2969dbc972502d7cb37f17410c12c2de1eaf7f

libkmod: strbuf_pushchars: handle pushing empty string gracefully
If buf->bytes, buf->used, and len are all 0, buf_grow() will do nothing,
and memcpy() willbe called with a NULL first argument. This will cause
an error because the function is annotated with __nonnull(1, 2).

Fixes: e62d8c7 ("strbuf: make strbuf_pushchars() a little less dumb")
Signed-off-by: Martin Wilck <martin_wilck@gmx.de>

shared/strbuf.c
@@ -100,6 +100,9 @@ size_t strbuf_pushmem(struct strbuf *buf, const char *src, size_t sz)
 	assert(src != NULL);
 	assert(buf != NULL);
 
+	if (sz == 0)
+		return 0;
+
 	if (!strbuf_reserve_extra(buf, sz))
 		return 0;
 

testsuite/test-strbuf.c
@@ -202,8 +202,7 @@ static int test_strbuf_pushmem(const struct test *t)
 
 	return 0;
 }
-DEFINE_TEST(test_strbuf_pushmem, .description = "test strbuf_reserve",
-	    .expected_fail = true);
+DEFINE_TEST(test_strbuf_pushmem, .description = "test strbuf_reserve");
 
 static int test_strbuf_used(const struct test *t)
 {